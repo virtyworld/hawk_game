7584f31da999a96c37263fba208a9a682d30127c
dima <ratash666@yahoo.com>
2021-09-21 14:58:17 +0300

    add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   
+    [SerializeField] private float smoothSpeed = 0.125f;
+    
+    [SerializeField] private Vector2 offset;
+    private Transform target;
+    
+    private void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            FollowTarget();
+        }
+      
+    }
+    
+    private void FollowTarget() {
+        Vector3 desirePosition = new Vector3(target.position.x + offset.x,
+            target.position.y + offset.y,transform.position.z);
+        transform.position = desirePosition;
+    }
+}
