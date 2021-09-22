c641bc66da7aedf4e97f8430434166d00a4d170b
dima <ratash666@yahoo.com>
2021-09-22 19:07:48 +0300

    change project from 2d to 3d add character model add script bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedStreamBullet : StreamBullet
+public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        firePoint = GameObject.FindWithTag("firePoint").transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        ShootStream();
+        
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleBullet : Bullet
+public class GameManager : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
-    
+
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        BulletMove();
+        
     }
 }
